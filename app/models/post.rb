4778e2c9e030d9113ea83dfc1064d86695b06c49
Dominic <dom42486@gmail.com>
2017-11-04 06:22:02 -0700

    referenced users to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ActiveRecord::Base
 
+  belongs_to :user
+  validates :user_id, presence: true
+  
   validates :image, presence: true
   has_attached_file :image
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :posts, dependent: :destroy
+
   has_attached_file :avatar, styles: { small: "100x100", med: "300x300", large: "500x500" }
   validates_attachment :avatar, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
